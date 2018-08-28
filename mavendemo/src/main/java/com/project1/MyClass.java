d3866954503e44c751011f4621141e8bba80161a
530918 <530918@08HW010493.NOAM.TCS.com>
2018-08-28 12:48:10 -0400

    initial checkin

---
+++ mavendemo/src/main/java/com/project1/MyClass.java
@@ -0,0 +1,8 @@
+package com.project1;
+
+public class MyClass {
+	public String sample()
+	{
+		return "gowtham";
+	}
+}
---
+++ mavendemo/src/test/java/com/project1/MyClassTest.java
@@ -0,0 +1,14 @@
+package com.project1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MyClassTest {
+
+	@Test
+	public void test() {
+		assertEquals("gowtham",new MyClass().sample());
+	}
+
+}
